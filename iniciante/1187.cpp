1038f85176b1e7e43db3f260d7ce05f25ff8f3c7
Laercio Rios <laercio.rios13@hotmail.com>
2021-12-28 14:34:25 -0300

    :sparkles: Solved 1187

---
+++ iniciante/1187.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+    double m[12][12];
+    char o;
+
+    cin >> o;
+
+    for(int i = 0; i < 12; i++)
+        for(int j = 0; j < 12;  j++)
+            cin >> m[i][j];
+
+    double sum = 0;
+    for(int i = 0; i < 12; i++){
+        for(int j = i+1; j < 11-i; j++) {
+            sum += m[i][j];
+        }
+    }
+
+    if(o == 'S')
+        printf("%.1lf\n", sum);
+    else
+        printf("%.1lf\n", sum/30);
+ 
+    return 0;
+}
